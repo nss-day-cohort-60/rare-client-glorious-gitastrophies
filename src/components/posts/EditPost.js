7256df793d6b6a214b5cedf762045fd9715451e4
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-03 14:16:15 -0600

    beginning of edit form

---
+++ src/components/posts/EditPost.js
@@ -0,0 +1,80 @@
+export const editPost = () => {
+
+
+
+    const [post, setPost] = useState({
+        title: "",
+        image_url : "",
+        content: ""
+    })
+
+    const handleInputChange = (event) => {
+        const copyOfPost = { ...post };
+        copyOfPost[event.target.id] = event.target.value;
+        setPost(copyOfPost);
+    };
+    const navigate = useNavigate()
+
+
+    const handleSubmit = (event) => {
+        console.log(post)
+
+        event.preventDefault();
+
+    return fetch("http://localhost:8088/posts", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+            })
+            .then((res) => res.json())
+            .then(() => {
+                navigate('/')
+            });
+        }
+
+
+    return (
+        <section>
+            <form className="postForm">
+            <h2>Create a Post</h2>
+            <fieldset>
+                <div className="form-group">
+                <label htmlFor="title">Title: </label>
+                <input type="text" name="title" id="title" required autoFocus className="form-control"
+                    placeholder="Title of Post"
+                    defaultValue={post.title}
+                    onChange={handleInputChange}
+                />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                <label htmlFor="content">Post Content: </label>
+                <textarea type="textbox" id="content" rows="5" cols="30" name="content" required autoFocus className="form-control" placeholder="Post Content" 
+                onChange={handleInputChange} 
+                />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                <label htmlFor="image_url">Add an Image:</label>
+                    <input
+                    required
+                    id="image_url"
+                    type="text"
+                    className="form-control"
+                    placeholder="Image URL here"
+                    onChange={handleInputChange}
+                    />
+                </div>
+            </fieldset>
+            <button type="submit"
+                onClick={handleSubmit}
+                className="btn btn-primary">
+            </button>
+            </form>
+        </section>
+    )
+}
